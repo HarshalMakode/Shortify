f80519cbff449d3203c96dbe72f9c6d677cf5331
HarshalMakode <harshalmakode26@gmail.com>
2026-09-18 07:57:20 +0530

    frontend half

--- Frontend/shortify/src/App.jsx
+++ Frontend/shortify/src/App.jsx
@@ -1,122 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className='min-h-screen flex flex-col bg-cyan-40'>
 
-      <div className="ticks"></div>
+      <header className='flex justify-center items-baseline w-full px-3'>
+        <span class="text-6xl font-bold text-blue-400 px-2 py-10">Shortify</span>
+        <span className='font-bold '>by</span>
+        <span className='ml-1 font-bold text-blue-600 '>MrHypix</span>
+      </header>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <main className='flex flex-col items-center p-4 gap-5'>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+        <section className='w-full max-w-3xl flex flex-col items-center bg-white py-5 rounded-2xl border border-mist-300 shadow-lg p-13'>
+          <span className='text-4xl font-bold text-gray-600'>Paste the URL to be shortened</span>
+          <form action="submit" className='w-full flex border border-gray-200 m-5 rounded shadow'>
+            <input type="text" className='flex-1 pl-5 py-2.5' placeholder='Enter the link' />
+            <button className='bg-blue-500 text-white p-2 rounded-r'>Shorten URL</button>
+          </form>
+          <span className='flex flex-col items-center'>
+            <span>Shortify is a free tool to shorten URLs and generate short links</span>
+            <span>URL shortener allows to create a shortened link making it easy to share</span>
+          </span>
+        </section>
+
+        <section className='w-full max-w-3xl flex flex-col items-center bg-white py-5 rounded-2xl border border-mist-300 shadow-lg p-10 gap-5'>
+          <span className='text-2xl font-bold text-gray-600'>Want More? Try Premium Features!</span>
+          <span className='flex flex-col items-center'>
+            <span>Custom short links, powerful dashboard, detailed analytics, API, UTM builder, QR codes,</span>
+            <span>browser extension, app integrations and support. Start Free</span>
+          </span>
+          <button className='bg-blue-500 text-white py-3 px-5 rounded'>Create Account</button>
+        </section>
+
+        <section>
+          <span>
+            <span>Simple and fast URL shortener!</span>
+            <span>ShortURL allows to shorten long links from Instagram, Facebook, YouTube, Twitter, Linked In, WhatsApp, TikTok, blogs and any domain name. Just paste the long URL and click the Shorten URL button. On the next page, copy the shortened URL and share it on sites, chat and emails. After shortening the URL, check how many clicks it received.</span>
+          </span>
+        </section>
+        
+        <section>
+          <span>
+            <span>Shorten, share and track</span>
+            <span>Your shortened URLs can be used in publications, documents, advertisements, blogs, forums, instant messages, and other locations. Track statistics for your business and projects by monitoring the number of hits from your URL with our click counter.</span>
+          </span>
+        </section>
+
+      </main>
+    </div>
   )
 }
 
-export default App
+export default App;
